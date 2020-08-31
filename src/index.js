f0873ed58f342bec6bed75c26136164265bf5539
Willem <willemhumphrey@gmail.com>
2020-08-31 14:12:47 -0700

    completed corona tracker test app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
+    document.querySelector('#root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//its important to use modules for css in react and if we dont then the styles will apply towards all code and that can be unpredictable so we want the css file to belong to the js file and we use modules for that
